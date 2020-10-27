b36e1e925ef63aed85b1f8f5711cc7d6f477c2c4
Will Kelly <willkelly9617@gmail.com>
2020-10-27 18:58:48 -0500

    Initial commit of projects

---
+++ README.md
@@ -0,0 +1,5 @@
+# Practice Projects
+
+This is a collection of projects I've built and am building for practice integrating HTML, CSS, and JS principles.  
+
+Some of them are taken from: [FlorinPop's list](https://github.com/florinpop17/app-ideas)
---
+++ binary-converter/app.js
@@ -0,0 +1,22 @@
+/*
+    Build a data structure that is a bio of yourself.
+    Include:
+    firstName
+    lastName
+    age
+    favFoods
+    favTV
+    pets
+    favMovies
+    ProgrammingLangs
+*/
+let will = {
+	firstName: "Will",
+    lastName: "Kelly",
+    age: 24,
+    favFoods: ['sweet-potatoes', 'ribs', 'tacos', 'pozole', 'most anything'],
+    favTV: 'whatever the wife wants to watch',
+    pets: 'not at this point',
+    favMovies: ['Starwars', 'nat']
+    ProgrammingLangs: ['html', 'css', 'js']
+}
